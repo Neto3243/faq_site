777a6e2043341926b5c263571a793f5a86cb05b1
Neto3243 <70845298+Neto3243@users.noreply.github.com>
2020-09-21 23:25:32 +0300

    html

--- faq/views.py
+++ faq/views.py
@@ -4,3 +4,17 @@ from user.models import Profile
 from django.views import generic
 
 
+def index(request):
+    num_quest = Question.objects.all().count()
+    num_user = Profile.objects.all().count()
+    num_answer = Answer.objects.all().count()
+
+    return render(
+        request,
+        'index.html',
+        context = {
+            'num_quest': num_quest,
+            'num_user': num_user,
+            'num_answer': num_answer
+        }
+    )
